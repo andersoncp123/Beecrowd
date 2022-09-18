5f2da68123d1239324fa3a87c5ba7bbb924a8a25
Anderson André <111925240+andersoncp123@users.noreply.github.com>
2022-09-18 09:34:32 -0300

    Create Problema 1041.py

---
+++ Problemas iniciantes/Problema 1041.py
@@ -0,0 +1,19 @@
+ponto = input(). split()
+
+x = float(ponto[0])
+y = float(ponto[1])
+
+if x == 0 and y == 0:
+    print("Origem")
+elif x == 0:
+    print("Eixo Y")
+elif y == 0:
+    print("Eixo X")
+elif x > 0 and y > 0:
+    print("Q1")
+elif y > 0 and x < 0:
+    print("Q2")
+elif x < 0 and y < 0:
+    print("Q3")
+elif x > 0 and y < 0:
+    print("Q4")
