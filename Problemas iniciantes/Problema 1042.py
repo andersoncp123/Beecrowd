2b2040e23377f28eec47142ea73a93ff271d7cec
Anderson André <111925240+andersoncp123@users.noreply.github.com>
2022-09-19 07:09:05 -0300

    Create Problema 1042.py

---
+++ Problemas iniciantes/Problema 1042.py
@@ -0,0 +1,16 @@
+val = input(). split()
+
+a = int(val[0])
+b = int(val[1])
+c = int(val[2])
+
+lista = [a, b, c]
+lista.sort()
+for x in lista:
+    print(x)
+
+print()
+
+print(a)
+print(b)
+print(c)
