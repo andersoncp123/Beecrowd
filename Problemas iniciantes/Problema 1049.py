1ec395b282673b74344d4fa45ad456c2ce6973bb
Anderson André <111925240+andersoncp123@users.noreply.github.com>
2022-09-27 13:39:50 -0300

    Create Problema 1049.py

---
+++ Problemas iniciantes/Problema 1049.py
@@ -0,0 +1,20 @@
+ani = input()
+ani2 = input()
+ani3 = input()
+
+if ani == "vertebrado" and ani2 == "ave" and ani3 == "carnivoro":
+    print("aguia")
+elif ani == "vertebrado" and ani2 == "ave" and ani3 == "onivoro":
+    print("pomba")
+elif ani == "vertebrado" and ani2 == "mamifero" and ani3 == "onivoro":
+    print("homem")
+elif ani == "vertebrado" and ani2 == "mamifero" and ani3 == "herbivoro":
+    print("vaca")
+elif ani == "invertebrado" and ani2 == "inseto" and ani3 == "hematofago":
+    print("pulga")
+elif ani == "invertebrado" and ani2 == "inseto" and ani3 == "herbivoro":
+    print("lagarta")
+elif ani == "invertebrado" and ani2 == "anelideo" and ani3 == "hematofago":
+    print("sanguessuga")
+elif ani == "invertebrado" and ani2 == "anelideo" and ani3 == "onivoro":
+    print("minhoca")
