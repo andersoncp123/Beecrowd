2233dc4da86e9ec72e969c6fbc2a238cfe30f719
Anderson André <111925240+andersoncp123@users.noreply.github.com>
2023-01-17 03:27:04 -0300

    Create Problema 1960.py

---
+++ Problemas iniciantes/Problema 1960.py
@@ -0,0 +1,43 @@
+num = int(input())
+
+while num > 0:
+    if num >= 1000:
+        print("M",end="")
+        num -= 1000
+    elif num >= 900:
+        print("CM",end="")
+        num -= 900
+    elif num >= 500:
+        print("D",end="")
+        num -= 500
+    elif num >= 400:
+        print("CD",end="")
+        num -= 400
+    elif num >= 100:
+        print("C",end="")
+        num -= 100
+    elif num >= 90:
+        print("XC",end="")
+        num -= 90
+    elif num >= 50:
+        print("L",end="")
+        num -= 50
+    elif num >= 40:
+        print("XL",end="")
+        num -= 40
+    elif num >= 10:
+        print("X",end="")
+        num -= 10
+    elif num >= 9:
+        print("IX",end="")
+        num -= 9
+    elif num >= 5:
+        print("V",end="")
+        num -= 5
+    elif num >= 4:
+        print("IV",end="")
+        num -= 4
+    elif num >= 1:
+        print("I",end="")
+        num -= 1
+print()
