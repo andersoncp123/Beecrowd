485837645398d4cfb75d76799820ac7756817dc1
Anderson André <111925240+andersoncp123@users.noreply.github.com>
2023-03-08 15:30:50 -0300

    Create Problema 2344.py

---
+++ Problemas iniciantes/Problema 2344.py
@@ -0,0 +1,12 @@
+nota = int(input())
+
+if nota == 0:
+    print("E")
+elif  1 <= nota <= 35:
+    print("D")
+elif 36 <= nota <= 60:
+    print("C")
+elif 61 <= nota <= 85:
+    print("B")
+else:
+    print("A")
